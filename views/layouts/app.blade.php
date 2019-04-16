2c8d26b6f28e9b0e03c4e3d9a7236d51a2c89c43
Duncan McClean <damcclean@users.noreply.github.com>
2019-04-16 22:32:34 +0100

    Fixed site title

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>{{ $title }} - @siteName()</title>
+    <title>{{ $title }} - {{ $config['siteName'] }}</title>
     <link rel="stylesheet" href="/css/app.css">
 
     <meta charset="UTF-8">
