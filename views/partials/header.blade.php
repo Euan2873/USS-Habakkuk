5417c4304204fa01675a722794b49600586b87e2
Duncan McClean <damcclean@users.noreply.github.com>
2019-04-16 22:33:19 +0100

    Fixed site title again

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="bg-black p-2 w-full">
         <div class="container mx-auto flex flex-row items-center justify-between">
             <a class="font-bold text-xl text-white hover:text-white no-underline" href="/">
-                @siteName()
+                {{ $config['siteName'] }}
             </a>
             <div class="flex flex-row items-center">
                 <a class="no-underline text-white hover:text-white m-2" href="/">
