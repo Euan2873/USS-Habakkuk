55235d8b761c66e301ac796d339307625f06349f
Duncan McClean <damcclean@users.noreply.github.com>
2019-04-18 10:05:30 +0100

    Fixed SEO meta information

--- views/partials/seo.blade.php
+++ views/partials/seo.blade.php
@@ -1,15 +1,15 @@
 <!-- Primary Meta Tags -->
-<meta name="title" content="@siteName()">
-<meta name="description" content="@config(description)">
+<meta name="title" content="{{ $config['siteName'] }}">
+<meta name="description" content="{{ $config['description'] }}">
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
 <meta property="og:url" content="/{{ $page['slug'] }}">
-<meta property="og:title" content="@siteName()">
-<meta property="og:description" content="@config(description)">
+<meta property="og:title" content="{{ $config['siteName'] }}">
+<meta property="og:description" content="{{ $config['description'] }}">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="/{{ $page['slug'] }}">
-<meta property="twitter:title" content="@siteName()">
-<meta property="twitter:description" content="@config(description)">
+<meta property="twitter:title" content="{{ $config['siteName'] }}">
+<meta property="twitter:description" content="{{ $config['description'] }}">
