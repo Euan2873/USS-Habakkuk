b3fb430bd9e05ce5704378dae1e910c5ebfc874e
Euan2873 <33867425+Euan2873@users.noreply.github.com>
2019-04-19 17:13:39 +0100

    Update episode.blade.php

--- views/partials/episode.blade.php
+++ views/partials/episode.blade.php
@@ -1,4 +1,4 @@
 <a class="md:w-1/4 bg-grey-lightest m-4 no-underline text-black rounded-6" href="{{ $episode['slug'] }}">
-    <img class="img-thumbnail" src="{{ $episode['image'] }}" alt="{{ $episode['title'] }}" style="width:200px;">
+    <img class="img-thumbnail" src="{{ $episode['image'] }}" alt="{{ $episode['title'] }}" style="width:300px;height:300px;">
     <h2 class="font-semibold text-2xl text-center">{{ $episode['title'] }}</h2>
 </a>
